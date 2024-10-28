9484080b2a6aa49833153bf5da57a25ba157d0fd
amjadsh92 <amjadsharafeddien@gmail.com>
2024-10-28 21:52:25 +0200

    retrieve dom elements

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+
+ var boundaries =  document.querySelectorAll(".boundary");
+ var start = document.getElementById("start");
+ var end = document.getElementById("end")
+ var status = document.getElementById("end")
+
+ 
